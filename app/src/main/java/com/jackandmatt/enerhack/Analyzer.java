f074832ae17376422266b4e46ed0003560ceca26
jlongsd <jlong25@wisc.edu>
2018-04-14 14:50:02 -0500

    client now connects to host and analyzer displays message received with data

--- app/src/main/java/com/jackandmatt/enerhack/Analyzer.java
+++ app/src/main/java/com/jackandmatt/enerhack/Analyzer.java
@@ -2,7 +2,7 @@ package com.jackandmatt.enerhack;
 /**
  * Created by Jack Long on 4/12/2018.
  */
-
+import java.util.concurrent.TimeUnit;
 public class Analyzer {
     private Client client;
     private boolean running;
@@ -18,15 +18,28 @@ public class Analyzer {
         running = true;
     }
 
-    public void run() {
+    public void run() throws InterruptedException {
         String message = "";
-        message = (packetAnalyzer("api/turnon/0"));
-        System.out.println(message);
+        int i = 0;
+        while(running) {
+            if(i < 9) {
+                message = packetAnalyzer("api/turnon/" + i);
+                System.out.println(message);
+                i++;
+            }
+            message = (packetAnalyzer("api/getnodestatus/all"));
+            System.out.println(message);
+            TimeUnit.SECONDS.sleep(5);
+        }
     }
 
     public String packetAnalyzer(String request) {
         String response = client.sendEcho(request);
         //Figure out how to analyze response packet into data you can use
         return response;
+    }
+
+    public void turnOff() {
+        this.running = false;
     }
 }
