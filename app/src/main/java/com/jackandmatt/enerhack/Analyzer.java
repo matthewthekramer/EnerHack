60c413332237206196eb2a941a29318ef5756f23
jlongsd <jlong25@wisc.edu>
2018-04-14 17:26:21 -0500

    before pull1

--- app/src/main/java/com/jackandmatt/enerhack/Analyzer.java
+++ app/src/main/java/com/jackandmatt/enerhack/Analyzer.java
@@ -1,11 +1,16 @@
 package com.jackandmatt.enerhack;
+
+
 /**
  * Created by Jack Long on 4/12/2018.
  */
 import java.util.concurrent.TimeUnit;
+
+
 public class Analyzer {
     private Client client;
     private boolean running;
+
 
 
     public void setRunning(boolean tOrf) {
@@ -18,28 +23,95 @@ public class Analyzer {
         running = true;
     }
 
-    public void run() throws InterruptedException {
-        String message = "";
-        int i = 0;
-        while(running) {
-            if(i < 9) {
-                message = packetAnalyzer("api/turnon/" + i);
-                System.out.println(message);
-                i++;
-            }
-            message = (packetAnalyzer("api/getnodestatus/all"));
-            System.out.println(message);
-            TimeUnit.SECONDS.sleep(5);
+    /**
+     * parses the values of the nodes from the JSON data
+     * @param msg
+     * @return
+     */
+    public double[] parseVals(String msg) {
+        double[] vals = new double[8];
+        int startArray = msg.indexOf('[');
+        int endarray = msg.indexOf(']');
+        String temp = msg.substring(startArray + 1, endarray);
+        String[] tempArray = temp.split(",");
+        for(int i = 0; i < 8; i++)
+        {
+            vals[i] = Double.parseDouble(tempArray[i]);
         }
+        return vals;
     }
 
+    /**
+     * calculates the total power consumption of all nodes
+     * @return
+     */
+    public double calcTotalPower() {
+        String message = packetAnalyzer("api/getacpoweractive/all");
+        //System.out.println(message);
+        double[] vals = this.parseVals(message);
+        double total = 0.0;
+        for(int i = 0; i < vals.length; i++)
+        {
+            total += total + vals[i];
+        }
+        return total;
+    }
+    /**
+     * calculates node that with highest power consumption
+     * @return
+     */
+    public int calcMostUsedNode() {
+        String message = packetAnalyzer("api/getacpoweractive/all");
+        //System.out.println(message);
+        double[] vals = this.parseVals(message);
+        double max = 0;
+        int maxNode = 0;
+        for(int i = 0; i < vals.length; i++)
+        {
+            if(vals[i] >= max)
+            {
+                max = vals[i];
+                maxNode = i;
+            }
+        }
+
+        return maxNode;
+    }
+    /**
+     * method that turns of a particular node
+     * @param node
+     */
+    public void turnOffNode(int node) {
+        String message = packetAnalyzer("api/turnoff/" + node);
+        System.out.println(message);
+    }
+    /**
+     * method that turns off all nodes
+     */
+    public void turnOffAll() {
+        String message = packetAnalyzer("api/turnoff/all");
+        System.out.println(message);
+    }
+
+    /**
+     * runs continuously updating total power consumption
+     * @throws InterruptedException
+     */
+    public void run() throws InterruptedException {
+        String message = "";
+        //message = (packetAnalyzer("api/getacpoweractive/all"));
+        System.out.println(message);
+        TimeUnit.SECONDS.sleep(5);
+
+    }
+    /**
+     * sends packets to server and analyzes what is received
+     * @param request
+     * @return
+     */
     public String packetAnalyzer(String request) {
         String response = client.sendEcho(request);
         //Figure out how to analyze response packet into data you can use
         return response;
-    }
-
-    public void turnOff() {
-        this.running = false;
     }
 }
--- app/src/main/java/com/jackandmatt/enerhack/MainActivity.java
+++ app/src/main/java/com/jackandmatt/enerhack/MainActivity.java
@@ -3,13 +3,21 @@ package com.jackandmatt.enerhack;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Object TextView;
+    private TextView mostused;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mostused = (TextView) findViewById(R.id.most_used_text);
+        int x = 0;
+        mostused.setText("Device " + x + "is costing you the most points.");
     }
 
     public void turn_off(View v){
