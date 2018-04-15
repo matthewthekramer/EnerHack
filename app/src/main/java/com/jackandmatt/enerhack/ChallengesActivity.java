1b6d2beb61b1b84fd98f790ffb11676680f07294
matthewthekramer <kramer22@wisc.edu>
2018-04-15 00:57:24 -0500

    added node picker class and functionality

--- app/src/main/java/com/jackandmatt/enerhack/ChallengesActivity.java
+++ app/src/main/java/com/jackandmatt/enerhack/ChallengesActivity.java
@@ -1,8 +1,10 @@
 package com.jackandmatt.enerhack;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ExpandableListView;
 
 import java.util.ArrayList;
@@ -42,37 +44,44 @@ public class ChallengesActivity extends AppCompatActivity {
         listDataChild = new HashMap<String, List<String>>();
 
         // Adding child data
-        listDataHeader.add("Top 250");
-        listDataHeader.add("Now Showing");
-        listDataHeader.add("Coming Soon..");
+        listDataHeader.add("Hour of Darkness - 20 Epts");
+        listDataHeader.add("1 Day 1 Node - 25 Epts");
+        listDataHeader.add("Better than Average - 5 Epts");
 
         // Adding child data
-        List<String> top250 = new ArrayList<String>();
-        top250.add("The Shawshank Redemption");
-        top250.add("The Godfather");
-        top250.add("The Godfather: Part II");
-        top250.add("Pulp Fiction");
-        top250.add("The Good, the Bad and the Ugly");
-        top250.add("The Dark Knight");
-        top250.add("12 Angry Men");
+        List<String> challenge1 = new ArrayList<String>();
+        challenge1.add("Go a full hour without using ANY electricity");
 
-        List<String> nowShowing = new ArrayList<String>();
-        nowShowing.add("The Conjuring");
-        nowShowing.add("Despicable Me 2");
-        nowShowing.add("Turbo");
-        nowShowing.add("Grown Ups 2");
-        nowShowing.add("Red 2");
-        nowShowing.add("The Wolverine");
+        List<String> challenge2 = new ArrayList<String>();
+        challenge2.add("Use only one node at a time for an entire day");
 
-        List<String> comingSoon = new ArrayList<String>();
-        comingSoon.add("2 Guns");
-        comingSoon.add("The Smurfs 2");
-        comingSoon.add("The Spectacular Now");
-        comingSoon.add("The Canyons");
-        comingSoon.add("Europa Report");
+        List<String> challenge3 = new ArrayList<String>();
+        challenge3.add("Beat the average american's power consumption in a day for the given devices you are using");
 
-        listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
-        listDataChild.put(listDataHeader.get(1), nowShowing);
-        listDataChild.put(listDataHeader.get(2), comingSoon);
+        listDataChild.put(listDataHeader.get(0), challenge1); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), challenge2);
+        listDataChild.put(listDataHeader.get(2), challenge3);
+    }
+
+    public void chart_onClick(View v){
+
+    }
+
+    public void home_onClick(View v){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void goal_onClick(View v){
+
+    }
+
+    public void challenges_onClick(View v){
+
+    }
+
+    public void impact_onClick(View v){
+
     }
 }
--- app/src/main/java/com/jackandmatt/enerhack/MainActivity.java
+++ app/src/main/java/com/jackandmatt/enerhack/MainActivity.java
@@ -10,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
 
     private Object TextView;
     private TextView mostused;
+    private Analyzer anal;
+    private int mostUsedNode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +20,13 @@ public class MainActivity extends AppCompatActivity {
 
         mostused = (TextView) findViewById(R.id.most_used_text);
         int x = 0;
-        mostused.setText("Device " + x + "is costing you the most points.");
+//        anal = new Analyzer();
+//        mostUsedNode = anal.calcMostUsedNode();
+//        mostused.setText("Device " + mostUsedNode + "is costing you the most points.");
     }
 
     public void turn_off(View v){
+//        anal.turnOffNode(mostUsedNode);
 
     }
 
@@ -34,7 +39,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void goal_onClick(View v){
-
+        Intent intent = new Intent(this, NodePickerActivity.class);
+        startActivity(intent);
     }
 
     public void challenges_onClick(View v){
